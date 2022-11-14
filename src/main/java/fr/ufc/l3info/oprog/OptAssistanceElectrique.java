e7795be3019a29fe79ffe61203a0484a6bd73685
Vadel <78354641+Vad3L@users.noreply.github.com>
2022-11-14 10:08:10 +0100

    Delete src directory

--- src/main/java/fr/ufc/l3info/oprog/OptAssistanceElectrique.java
+++
@@ -1,9 +0,0 @@
-package fr.ufc.l3info.oprog;
-
-public class OptAssistanceElectrique extends Option{
-
-    public OptAssistanceElectrique(IVelo o) {
-        super(o,2,"assistance électrique");
-    }
-
-}
--- src/main/java/fr/ufc/l3info/oprog/OptFreinsDisque.java
+++
@@ -1,9 +0,0 @@
-package fr.ufc.l3info.oprog;
-
-public class OptFreinsDisque  extends Option{
-
-    public OptFreinsDisque(IVelo o) {
-        super(o,0.3,"freins à disque");
-    }
-
-}
